ebe949d75d7c253fcbdd99b6008fdfae3afa9d57
Valerio Bozzolan <boz@reyboz.it>
2021-02-05 11:19:14 +0000

    show license in cronos.js

--- www/static/cronos.js
+++ www/static/cronos.js
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2020, 2021 Valerio Bozzolan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt
+ */
 ( function() {
 
 	function addHiddenInput( form, name, value ) {
